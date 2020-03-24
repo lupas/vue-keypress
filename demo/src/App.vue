66c253539661a79434d07802ccadccc1d58684d1
Pascal <pascal@luther.ch>
2020-03-24 14:46:53 +0100

    added demo

---
+++ demo/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <Keypress event="keyup" @pressed="someMethod" />
+    <div class="card" :style="dynamicBackgroundStyle">
+      <p class="keyCode">{{pressedKeyCode}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Keypress: () => import('vue-keypress')
+  },
+  data: () => ({
+    pressedKeyCode: null
+  }),
+  computed: {
+    dynamicBackgroundStyle() {
+      if (!this.pressedKeyCode) {
+        return { 'backgroundImage': "url('/keypressLogo.png')" }
+      } else {
+        return { 'backgroundImage': "url('/emptyKey.png')" }
+      }
+      
+    }
+  },
+  methods: {
+    someMethod(val) {
+      this.pressedKeyCode = val
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.card {
+  background-size: contain;
+  background-repeat: no-repeat;
+  height: 40vh;
+  width: 40vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+.keyCode {
+  font-size: 70px;
+  font-weight: 600;
+  margin-bottom: 40%;
+  margin-right: 3%;
+}
+</style>
