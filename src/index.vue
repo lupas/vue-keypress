e59c7a09f7e7a6259abcfa205158b21f09b8f28c
niuage <niuage@gmail.com>
2020-12-25 22:54:55 +0100

    Rewrote the component using the composition API.

--- src/index.vue
+++ src/index.vue
@@ -1,125 +1,130 @@
 <script>
-const supportedModifiers = ['altKey', 'metaKey', 'ctrlKey', 'shiftKey']
+  const supportedModifiers = ['altKey', 'metaKey', 'ctrlKey', 'shiftKey'];
 
-export default {
-  props: {
-    keyEvent: {
-      type: String,
-      default: 'keyup',
+  import { ref, onMounted, onUnmounted } from 'vue';
+
+  export default {
+    name: "Keypress",
+
+    props: {
+      keyEvent: {
+        type: String,
+        default: 'keyup',
+      },
+      // Only for single key code:
+      keyCode: {
+        type: Number,
+        default: null,
+      },
+      modifiers: {
+        type: Array, // ['shiftKey', 'ctrlKey', 'altKey', 'metaKey']
+        default: () => [],
+      },
+      preventDefault: {
+        type: Boolean,
+      },
+      // Only for multiple key codes:
+      multipleKeys: {
+        type: Array,
+        default: () => [],
+      },
     },
 
-    // Only for single key code:
-    keyCode: {
-      type: Number,
-      default: null,
-    },
-    modifiers: {
-      type: Array, // ['shiftKey', 'ctrlKey', 'altKey', 'metaKey']
-      default: () => [],
-    },
-    preventDefault: {
-      type: Boolean,
-    },
+    setup(props, { emit }) {
+      const keyListeners = [];
 
-    // Only for multiple key codes:
-    multipleKeys: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  data: () => ({
-    keyListeners: [],
-  }),
-  mounted() {
-    this.setupListeners()
-  },
-  destroyed() {
-    for (const { keyEvent, listener } of this.keyListeners) {
-      window.removeEventListener(keyEvent, listener)
-    }
-  },
-  methods: {
-    /** Initial Setup of the listeners */
-    /** ****************************** */
-    setupListeners() {
-      const expectedEvent = {
-        keyEvent: this.keyEvent,
+      /** Initial Setup of the listeners */
+      /** ****************************** */
+      const setupListeners = () => {
+        const expectedEvent = {
+          keyEvent: props.keyEvent,
+          // If single:
+          keyCode: props.keyCode,
+          preventDefault: props.preventDefault,
+          modifiers: props.modifiers,
+          // If multiple:
+          multipleKeys: props.multipleKeys,
+        }
 
-        // If single:
-        keyCode: this.keyCode,
-        preventDefault: this.preventDefault,
-        modifiers: this.modifiers,
-        // If multiple:
-        multipleKeys: this.multipleKeys,
+        addEventListener(expectedEvent)
       }
-      this.addEventListener(expectedEvent)
-    },
-    addEventListener(expectedEvent) {
-      let listener = this.eventHandler(expectedEvent)
-      window.addEventListener(expectedEvent.keyEvent, listener)
-      this.keyListeners.push({ expectedEvent, listener })
-    },
 
-    /** Handling per keypress event */
-    /** *************************** */
-    eventHandler(expectedEvent) {
-      return (event) => {
-        const emitResponse = (emitEvent, message) => {
-          this.$emit(emitEvent, {
-            event,
-            expectedEvent,
-            message,
-          })
+      const addEventListener = (expectedEvent) => {
+        let listener = eventHandler(expectedEvent)
+        window.addEventListener(expectedEvent.keyEvent, listener)
+        console.log(expectedEvent)
+        keyListeners.push({ expectedEvent, listener })
+      }
+
+      const removeListeners = () => {
+        for (const { expectedEvent, listener } of keyListeners) {
+          window.removeEventListener(expectedEvent.keyEvent, listener)
         }
-        // Emit the emitEvent '@any' in any case:
-        emitResponse('any', 'Any key was pressed.')
+      }
 
-        const inMultipleKeysMode = expectedEvent.multipleKeys.length > 0
+      /** Handling per keypress event */
+      /** *************************** */
+      const eventHandler = (expectedEvent) => {
+        return (event) => {
+          const emitResponse = (emitEvent, message) => {
+            emit(emitEvent, {
+              event,
+              expectedEvent,
+              message,
+            })
+          }
+          // Emit the emitEvent '@any' in any case:
+          emitResponse('any', 'Any key was pressed.')
 
-        // In "any key" mode, emit '@success' and return
-        const inAnyKeyMode = !expectedEvent.keyCode && !inMultipleKeysMode
-        if (inAnyKeyMode) {
-          emitResponse('success', 'Any key was pressed.')
-          return
-        }
+          const inMultipleKeysMode = expectedEvent.multipleKeys.length > 0
 
-        // Set expected inputs array respective to props bein "single" or "multiple"
-        let expectedInputs = [expectedEvent]
-        if (inMultipleKeysMode) {
-          expectedInputs = expectedEvent.multipleKeys
-        }
-
-        for (const expectedInput of expectedInputs) {
-          // Check if the correct keys have been clicked:
-          const correctKeyPressed = expectedInput.keyCode === event.keyCode
-          if (!correctKeyPressed) continue
-
-          // Get modifiers:
-          let hasModifiers = expectedInput.modifiers.length > 0
-
-          // Check if only the specified modifiers were pressed
-          if (hasModifiers) {
-            const modifiersPressed = supportedModifiers.every(
-              (x) => event[x] == (expectedInput.modifiers.indexOf(x) !== -1)
-            )
-            if (!modifiersPressed) continue
+          // In "any key" mode, emit '@success' and return
+          const inAnyKeyMode = !expectedEvent.keyCode && !inMultipleKeysMode
+          if (inAnyKeyMode) {
+            emitResponse('success', 'Any key was pressed.')
+            return
           }
 
-          // SUCCESS -> if it got to here, this was the correct key.
+          // Set expected inputs array respective to props bein "single" or "multiple"
+          let expectedInputs = [expectedEvent]
 
-          // Set Prevent-Default
-          if (expectedEvent.preventDefault) {
-            event.preventDefault()
+          if (inMultipleKeysMode) {
+            expectedInputs = expectedEvent.multipleKeys
           }
-          emitResponse('success', 'Correct key(s) pressed.')
-          return
-        }
 
-        // FAILURE: If it got to here, the correct key wasn't pressed:
-        emitResponse('wrong', 'Wrong key(s) pressed.')
+          for (const expectedInput of expectedInputs) {
+            // Check if the correct keys have been clicked:
+            const correctKeyPressed = expectedInput.keyCode === event.keyCode
+            if (!correctKeyPressed) continue
+
+            // Get modifiers:
+            let hasModifiers = expectedInput.modifiers.length > 0
+            // Check if only the specified modifiers were pressed
+            if (hasModifiers) {
+              const modifiersPressed = supportedModifiers.every(
+                (x) => event[x] == (expectedInput.modifiers.indexOf(x) !== -1)
+              )
+              if (!modifiersPressed) continue
+            }
+
+            // SUCCESS -> if it got to here, this was the correct key.
+            // Set Prevent-Default
+            if (expectedEvent.preventDefault) {
+              event.preventDefault()
+            }
+            emitResponse('success', 'Correct key(s) pressed.')
+            return
+          }
+
+          // FAILURE: If it got to here, the correct key wasn't pressed:
+          emitResponse('wrong', 'Wrong key(s) pressed.')
+        }
       }
+
+      onMounted(() => setupListeners());
+      onUnmounted(() => removeListeners());
     },
-  },
-  render: () => null,
-}
+
+    render() { null },
+  }
 </script>
